6a9d3782477d3e523e8539b96536d6a3477d7b4e
Riccardo <riccardo.pomato@maersk.com>
2020-01-10 15:32:07 +0000

    Update Readme file

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # pods-autoregister-dns
 This project is about a sidecar container to autoregister a Kubernetes POD IP address in DNS cloud provider
+
+The container is a python script.
+When it start, it gets the POD ip and then register an A record in the DNS zone using the POD ip.
+
+This container support only Azure Public DNS zone and Azure Private DNS Zone.
+I am planning to add more support to different cloud like Google, AWS and more.
+Please if you interested, feel free to change the code or add more cloud support using pull request.
+
+The ENV variables mandatory are:
+
+DnsProvider: "AzureDNS"
+subscription_id: "xxxxxxxx-xxxx-xxxxx-xxxxxxxxxxxxxxxxx"
+ResourceGroup: "resourcegroupname"
+DnsZone: "domainname.com"
+TENANT_ID: "xxxxxxxx-xxxx-xxxx-xxxxxxxxxxxxxxxxx"
+CLIENT: "xxxxxxxx-xxxx-xxxx-xxxxxxxxxxxxxxxxx"
+KEY: "xxxxxxxx-xxxx-xxxx-xxxxxxxxxxxxxxxxx"
+
+DnsProvider can have only two values (case sensitive): AzureDNS or AzurePrivateDNS
+subscription_id: is referring to the subscrption that host the DNS zone
+ResourceGroup: is referring to the Resource Group that contain the DNS zone
+DnsZone: name of the DNS zone
+TENANT_ID: is the tenant where we need to log in using the service principal
+CLIENT: is the AppId of the Service Principal
+KEY: is the Service Principal secret
+
+In kubernetes deployment, is good to create a configmap and store the ENV variable like this example:
+
+kind: ConfigMap
+apiVersion: v1
+metadata:
+  name: appconfig
+data:
+        DnsProvider: "AzurePrivateDNS"
+        subscription_id: "xxxxxxxx-xxxx-xxxxx-xxxxxxxxxxxxxxxxx"
+        ResourceGroup: "resourcegroupname"
+        DnsZone: "domainname.local"
+        TENANT_ID: "xxxxxxxx-xxxx-xxxxx-xxxxxxxxxxxxxxxxx"
+        CLIENT: "xxxxxxxx-xxxx-xxxxx-xxxxxxxxxxxxxxxxx"
+        KEY: "xxxxxxxx-xxxx-xxxxx-xxxxxxxxxxxxxxxxx"
---
+++ sidecar-dns-registration-env/dnsregister.py
@@ -0,0 +1,157 @@
+# DNSregister is a container that used to register a DNS A record in a DNS cloud provider
+
+  
+# Importing library 
+import logging
+import socket 
+import time
+import signal
+from os import environ
+from azure.mgmt.dns import DnsManagementClient
+from azure.mgmt.privatedns import PrivateDnsManagementClient
+from azure.common.credentials import ServicePrincipalCredentials
+
+# Load the environment variable
+subscription_id = environ.get('subscription_id')
+dnsProvider = environ.get('DnsProvider')
+rg = environ.get('ResourceGroup')
+dnszone = environ.get('DnsZone')
+
+# Set the logging detail level
+logging.basicConfig(level=logging.INFO)
+logging.info('DNS provider: %s', dnsProvider)
+
+class GracefulKiller:
+    kill_now = False
+    signals = {
+        signal.SIGINT: 'SIGINT',
+        signal.SIGTERM: 'SIGTERM'
+    }
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+    def exit_gracefully(self, signum, frame):
+        (hostname,ip)=get_Host_name_IP()
+        logging.info("Deregister the A %s record...", hostname)
+#   Remove the A record based to the DNS cloud provider
+        if dnsProvider=='AzureDNS':
+            logging.info("AzureDNS removing record...")
+            cred=connect2Azure()
+            DeleteAzureDNS_record(cred, subscription_id, rg, dnszone, hostname)
+            logging.info("Record DNS removed")
+        elif dnsProvider=='AzurePrivateDNS':
+            logging.info("AzurePrivateDNS removing record...")
+            cred=connect2Azure()
+            DeleteAzurePrivateDNS_record(cred, subscription_id, rg, dnszone, hostname)
+            logging.info("Record DNS removed")
+        self.kill_now = True
+
+def connect2Azure():
+    # Load the environment variable to determinate the security context
+
+    # Tenant ID for your Azure subscription
+    TENANT_ID = environ.get('TENANT_ID') 
+    # Your service principal App ID
+    CLIENT = environ.get('CLIENT') 
+    # Your service principal password
+    KEY = environ.get('KEY') 
+    try:
+        credentials = ServicePrincipalCredentials(
+                client_id = CLIENT,
+                    secret = KEY,
+                        tenant = TENANT_ID)
+        logging.info("Connection to Azure estabilished")
+    except Exception as e:
+        logging.error("Exception occurred during Azure connection!", exc_info=True)
+    return (credentials)
+
+def CreateAzureDNS_record(credentials, subscription_id, rg, dnszone, hostname, ip):
+    try:
+        dns_client = DnsManagementClient(
+                credentials,
+                    subscription_id
+                    )
+        record_set = dns_client.record_sets.create_or_update(
+            rg,
+            dnszone,
+            hostname,
+            'A',
+            {
+                "ttl": 300,
+                "arecords": [
+                {
+                    "ipv4_address": ip
+                }]
+            }
+        )
+        logging.info("AzureDNS record %s created", hostname)
+    except Exception as e:
+          logging.error("Exception occurred during AzureDNS record creation", exc_info=True)
+
+def CreateAzurePrivateDNS_record(credentials, subscription_id, rg, dnszone, hostname, ip):
+    try:
+        dns_client = PrivateDnsManagementClient(
+            credentials,
+            subscription_id
+            )
+        record_set = dns_client.record_sets.create_or_update(
+            rg,
+            dnszone,
+            'A',
+            hostname,
+            {
+                "ttl": 300,
+                "arecords": [
+                {
+                    "ipv4_address": ip
+                }]
+            }
+        )
+        logging.info("AzurePrivateDNS record %s created", hostname)
+    except Exception as e:
+        logging.error("Exception occurred during AzurePrivateDNS record creation", exc_info=True)
+
+def DeleteAzurePrivateDNS_record(credentials, subscription_id, rg, dnszone, host):
+    try:
+        dns_client = PrivateDnsManagementClient(credentials, subscription_id)
+        record_set = dns_client.record_sets.delete(rg, dnszone,'A', host)
+        logging.info("AzurePrivateDNS record %s removed", host)
+    except Exception as e:
+        logging.error("Exception occurred during AzurePrivateDNS record deletion", exc_info=True)
+
+def DeleteAzureDNS_record(credentials, subscription_id, rg, dnszone, host):
+    try:
+        dns_client = DnsManagementClient(credentials, subscription_id)
+        record_set = dns_client.record_sets.delete(rg, dnszone, host, 'A')
+        logging.info("AzureDNS record %s removed", host)
+    except Exception as e:
+        logging.error("Exception occurred during AzurePrivateDNS record deletion", exc_info=True)
+
+# Function to get hostname and IP address 
+def get_Host_name_IP(): 
+    try: 
+        host_name = socket.gethostname() 
+        host_ip = socket.gethostbyname(host_name)
+        logging.info("Getting pod info...")
+        logging.info("Hostname : %s", host_name)
+        logging.info("IP : %s", host_ip)
+        return (host_name,host_ip)
+    except Exception as e: 
+        logging.error("Unable to get Hostname and IP", exc_info=True)
+
+# Check which DNS provider has been selected
+(hostname,ip)=get_Host_name_IP() 
+if dnsProvider=='AzureDNS':
+    cred=connect2Azure()
+    CreateAzureDNS_record(cred, subscription_id, rg, dnszone, hostname, ip)
+elif dnsProvider=='AzurePrivateDNS':
+    cred=connect2Azure()
+    CreateAzurePrivateDNS_record(cred, subscription_id, rg, dnszone, hostname, ip)
+
+
+if __name__ == '__main__':
+    killer = GracefulKiller()
+    logging.info("Running...")
+    while not killer.kill_now:
+        time.sleep(1)
+
